332d8ae3bbd367b0d2584d85e187b0725a1cfe82
Nathan Addison <itmademe49@gmail.com>
2025-01-20 16:22:42 +1300

    add browserrouter basename

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => {
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename="/TextOcean_App">
             <Routes>
               <Route path="/" element={<Index />} />
             </Routes>
