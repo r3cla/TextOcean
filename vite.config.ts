6afe2d282645892c614d071cae4dabe9c7fecf87
Nathan Addison <itmademe49@gmail.com>
2025-01-20 16:50:58 +1300

    fix path for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: '/TextOcean/',  // Added this line
+  base: '/',
   server: {
     host: "::",
     port: 8080,
