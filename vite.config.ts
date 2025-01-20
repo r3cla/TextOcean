8108b6cf59494aa67a09b8dbd67abf2de12877b5
Nathan Addison <itmademe49@gmail.com>
2025-01-20 15:44:38 +1300

    update path name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
+  base: '/TextOcean_App/',  // Added this line
   server: {
     host: "::",
     port: 8080,
