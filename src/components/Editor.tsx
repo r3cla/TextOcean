933fa04092ede9cd48c49c68ebf2375af7084c02
Nathan Addison <itmademe49@gmail.com>
2025-01-23 00:29:56 +1300

    add auto formatting for css, js and html

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,7 +1,14 @@
-import MonacoEditor from "@monaco-editor/react";
+import MonacoEditor, { Monaco } from "@monaco-editor/react";
 import { EditorProps } from "@/types/editor";
 
 const Editor = ({ content, onChange, language, isRawView, height = "460px", theme = "vs-dark" }: EditorProps) => {
+  const handleEditorMount = (editor: any, monaco: Monaco) => {
+    // Add Format Document command
+    editor.addCommand(monaco.KeyMod.Alt | monaco.KeyMod.Shift | monaco.KeyCode.KeyF, () => {
+      editor.getAction('editor.action.formatDocument').run();
+    });
+  };
+
   const renderRawView = () => (
     <pre className="p-4 pr-8 whitespace-pre-wrap break-words font-mono text-sm" style={{ height }}>
       {content}
@@ -9,28 +16,31 @@ const Editor = ({ content, onChange, language, isRawView, height = "460px", them
   );
 
   const renderMonacoEditor = () => (
-      <MonacoEditor
-        height={height}
-        language={language}
-        value={content}
-        onChange={(value) => onChange(value ?? "")}
-        theme={theme}
-        options={{
-          minimap: { enabled: false },
-          fontSize: 14,
-          wordWrap: "on",
-          padding: { top: 16 },
-          scrollbar: {
-            vertical: 'visible',
-            horizontalSliderSize: 12,
-            verticalSliderSize: 12,
-            horizontal: 'visible',
-            useShadows: true,
-            verticalScrollbarSize: 16,
-            horizontalScrollbarSize: 16
-          }
-        }}
-      />
+    <MonacoEditor
+      height={height}
+      language={language}
+      value={content}
+      onChange={(value) => onChange(value ?? "")}
+      theme={theme}
+      onMount={handleEditorMount}
+      options={{
+        minimap: { enabled: false },
+        fontSize: 12,
+        wordWrap: "on",
+        padding: { top: 16 },
+        formatOnPaste: true,
+        formatOnType: true,
+        scrollbar: {
+          vertical: 'visible',
+          horizontalSliderSize: 12,
+          verticalSliderSize: 12,
+          horizontal: 'visible',
+          useShadows: true,
+          verticalScrollbarSize: 16,
+          horizontalScrollbarSize: 16
+        }
+      }}
+    />
   );
 
   return isRawView ? renderRawView() : renderMonacoEditor();
