bd66b85455fcf3bbfb02bfdd30360a00368e3138
Nathan Addison <itmademe49@gmail.com>
2025-01-22 16:50:18 +1300

    remove ts, py, markdown from supported syntax languages, so we can focus on implementing better highlighting for supported languages first

--- src/constants/languages.ts
+++ src/constants/languages.ts
@@ -1,11 +1,13 @@
 import { Language } from "@/types/editor";
 
+interface Language {
+  value: SupportedLanguage;
+  label: string;
+}
+
 export const SUPPORTED_LANGUAGES: Language[] = [
-  { value: "typescript", label: "TypeScript" },
   { value: "javascript", label: "JavaScript" },
-  { value: "python", label: "Python" },
   { value: "html", label: "HTML" },
   { value: "css", label: "CSS" },
-  { value: "markdown", label: "Markdown" },
-  { value: "plaintext", label: "Plain Text" },
+  { value: "plaintext", label: "None" },
 ];
